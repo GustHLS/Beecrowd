574616aac0d8dd10914e7dc4148bc2845629d215
Gustavo Santinho <88120522+GustHLS@users.noreply.github.com>
2023-10-09 19:27:13 -0300

    Extremamente Básico

---
+++ C++/Iniciante/1001.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+ 
+int main() {
+ 
+    int a, b, soma;
+    
+    std::cin >> a;
+    std::cin >> b;
+    
+    soma = a + b;
+    
+    printf("X = %d\n", soma);
+ 
+    return 0;
+}
