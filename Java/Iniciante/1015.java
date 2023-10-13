54dadccaf9617649fe4c914d1d18bcefcec254fb
Gustavo Santinho <88120522+GustHLS@users.noreply.github.com>
2023-10-13 00:01:14 -0300

    Distância Entre Dois Pontos

---
+++ Java/Iniciante/1015.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+  
+    public static void main(String[] args) throws IOException {
+      Scanner scanner = new Scanner(System.in);
+
+      double x1 = scanner.nextDouble();
+      double y1 = scanner.nextDouble();
+      double x2 = scanner.nextDouble();
+      double y2 = scanner.nextDouble();
+
+      double distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+      String resultado = String.format("%.4f", distancia);
+      System.out.println(resultado);
+    }
+}
+
