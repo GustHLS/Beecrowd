0ad670ebcfaf72c86e771ae3c4239f897c9de127
Gustavo Santinho <88120522+GustHLS@users.noreply.github.com>
2023-10-17 11:14:17 -0300

    Distância

---
+++ C#/Iniciante/1016.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int km = Convert.ToInt32(Console.ReadLine());
+        int minutos = km * 2;
+        
+        Console.WriteLine($"{minutos} minutos");
+
+    }
+
+}
