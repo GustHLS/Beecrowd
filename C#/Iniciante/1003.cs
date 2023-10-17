d7b12bfe1fa73e3d6ae30fca0301458ca524df45
Gustavo Santinho <88120522+GustHLS@users.noreply.github.com>
2023-10-17 11:11:27 -0300

    Soma Simples

---
+++ C#/Iniciante/1003.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        int SOMA = A + B;
+
+        Console.WriteLine($"SOMA = {SOMA}");
+
+    }
+
+}
